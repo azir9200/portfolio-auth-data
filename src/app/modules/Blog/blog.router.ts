f82aca92972b444f802bd7d46d52aae063de8b43
Azir@9200 <aziruddin83@hotmail.com>
2025-10-18 20:47:41 +0100

    user info updated

--- src/app/modules/Blog/blog.router.ts
+++ src/app/modules/Blog/blog.router.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { BlogController } from "./blog.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
 
 const router = express.Router();
 
-router.post("/", BlogController.createBlog);
+router.post("/", checkAuth("OWNER"), BlogController.createBlog);
 router.get("/", BlogController.getAllBlog);
 router.get("/:id", BlogController.getBlogById);
 router.patch("/:id", BlogController.updateBlog);
--- src/app/modules/Project/project.router.ts
+++ src/app/modules/Project/project.router.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { ProjectController } from "./project.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
 
 const router = express.Router();
 
-router.post("/", ProjectController.createProject);
-router.get("/", ProjectController.getAllProject);
+router.post("/", checkAuth("OWNER"), ProjectController.createProject);
+router.get("/", checkAuth("OWNER"), ProjectController.getAllProject);
 router.get("/:id", ProjectController.getProjectById);
 router.patch("/:id", ProjectController.updateProject);
 router.delete("/:id", ProjectController.deleteProject);
